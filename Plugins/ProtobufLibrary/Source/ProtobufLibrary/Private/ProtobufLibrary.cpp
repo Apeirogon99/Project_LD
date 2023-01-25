bdf15c9f611f1fe1ddbce3a232fa600eeac0ce65
Apeirogon99 <Apeirogon99@gmail.com>
2023-01-25 20:04:40 +0900

    [gwanho] Add Protobuf plugins
    타 라이브러리 사용하려면 플러그인 만들어서 넣으라고 언리얼 문서에 있어서 했는데 나중에 수정사항 있을수도 있음

---
+++ Plugins/ProtobufLibrary/Source/ProtobufLibrary/Private/ProtobufLibrary.cpp
@@ -0,0 +1,22 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProtobufLibrary.h"
+#include "Core.h"
+#include "Modules/ModuleManager.h"
+//#include "Interfaces/IPluginManager.h"
+
+#define LOCTEXT_NAMESPACE "FProtobufLibraryModule"
+
+void FProtobufLibraryModule::StartupModule()
+{
+
+}
+
+void FProtobufLibraryModule::ShutdownModule()
+{
+
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FProtobufLibraryModule, ProtobufLibrary)
---
+++ Plugins/ProtobufLibrary/Source/ProtobufLibrary/ProtobufLibrary.Build.cs
@@ -0,0 +1,69 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using System;
+using System.IO;
+using UnrealBuildTool;
+
+public class ProtobufLibrary : ModuleRules
+{
+	public ProtobufLibrary(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        string ThirdPartyDirectory = Path.Combine(ModuleDirectory, "../ThirdParty/");
+        string ProtobufIncludePath = Path.Combine(ThirdPartyDirectory, "ProtobufLibrary/include");
+        string ProtobufLibraryPath = Path.Combine(ThirdPartyDirectory, "ProtobufLibrary/");
+
+        PublicDefinitions.Add("WITH_PROTOBUFLIBRARY=1");
+
+        Console.WriteLine("Configuration = " + Target.Configuration + "Platform = " + Target.Platform);
+
+        if (true == IsDebug(Target.Configuration))
+        {
+            ProtobufLibraryPath += "debug/lib/";
+            PublicAdditionalLibraries.Add(Path.Combine(ProtobufLibraryPath, "libprotobufd.lib"));
+            PublicAdditionalLibraries.Add(Path.Combine(ProtobufLibraryPath, "libprotobuf-lited.lib"));
+            PublicAdditionalLibraries.Add(Path.Combine(ProtobufLibraryPath, "libprotocd.lib"));
+        }
+        else
+        {
+            ProtobufLibraryPath += "Release/lib/";
+            PublicAdditionalLibraries.Add(Path.Combine(ProtobufLibraryPath, "libprotobuf.lib"));
+            PublicAdditionalLibraries.Add(Path.Combine(ProtobufLibraryPath, "libprotobuf-lite.lib"));
+            PublicAdditionalLibraries.Add(Path.Combine(ProtobufLibraryPath, "libprotoc.lib"));
+        }
+
+        PublicSystemIncludePaths.Add(ProtobufIncludePath);
+
+        PublicDependencyModuleNames.AddRange
+            (
+                new string[]
+                {
+                            "Core",
+                }
+            );
+
+        PublicDefinitions.AddRange(
+            new string[]
+            {
+                "GOOGLE_PROTOBUF_NO_RTTI=1",
+                "GOOGLE_PROTOBUF_USE_UNALIGNED=0",
+                "_CRT_SECURE_NO_WARNINGS"
+            });
+
+        bEnableUndefinedIdentifierWarnings = false;
+        bEnableExceptions = true;
+    }
+
+    public bool IsDebug(UnrealTargetConfiguration Configuration)
+    {
+        if (Configuration == UnrealTargetConfiguration.Debug || Configuration == UnrealTargetConfiguration.DebugGame)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
