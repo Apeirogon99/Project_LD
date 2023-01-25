48653ae0f83bfbd048f7f32b016b71b917d8b6a3
Apeirogon99 <Apeirogon99@gmail.com>
2023-01-25 20:10:38 +0900

    [gwanho] Continuous Updates Protocol
    앞으로 지속적으로 업데이트가 이루어질 Packet과 Handle임

---
+++ Source/Project_LD/Public/Protobuf/Handler/IdentityClientPacketHandler.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include <IdentityPacket.pb.h>
+#include "IdentityClientPacketHandler.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_LD_API UIdentityClientPacketHandler : public UObject
+{
+	GENERATED_BODY()
+	
+};
