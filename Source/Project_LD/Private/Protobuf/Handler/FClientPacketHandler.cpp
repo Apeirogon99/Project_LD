a198be1e1f21f74e62e51768084977f9d44c3de6
Apeirogon99 <Apeirogon99@gmail.com>
2023-01-30 17:35:50 +0900

    [gwanho] Protobuf 업데이트
    아직 미완성 이거먼저 다시 수정할듯

---
+++ Source/Project_LD/Private/Protobuf/Handler/FClientPacketHandler.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Protobuf/Handler/FClientPacketHandler.h"
+
+FClientPacketHandler::FClientPacketHandler()
+{
+}
+
+FClientPacketHandler::~FClientPacketHandler()
+{
+}
--- Source/Project_LD/Private/Protobuf/Handler/IdentityClientPacketHandler.cpp
+++ Source/Project_LD/Private/Protobuf/Handler/IdentityClientPacketHandler.cpp
@@ -2,4 +2,61 @@
 
 
 #include "Protobuf/Handler/IdentityClientPacketHandler.h"
+#include <Framework/Gameinstance/LDGameInstance.h>
+#include <Network/NetworkCharacter.h>
+#include <Network/NetworkGameMode.h>
+#include <Network/NetworkGameState.h>
+#include <Network/NetworkPawn.h>
+#include <Network/NetworkPlayerState.h>
+#include <Network/NetworkService.h>
+#include <Network/NetworkUtils.h>
+#include <Network/NetworkSession.h>
 
+#include <Framework/Identity/IdentityGameMode.h>
+#include <Framework/Identity/IdentityPlayerController.h>
+
+//PacketHandlerFunc GPacketHandler[UINT16_MAX];
+
+bool Handle_INVALID(ANetworkController* session, BYTE* buffer, int32 len)
+{
+	UNetworkUtils::NetworkConsoleLog("PacketHandle : Handle_INVALID", ELogLevel::Error);
+	return true;
+}
+
+bool Handle_S2C_Singin(ANetworkController* session, Protocol::S2C_Singin& pkt)
+{
+	AIdentityPlayerController* controller = Cast<AIdentityPlayerController>(session);
+	if (nullptr == controller)
+	{
+		return false;
+	}
+
+	ULDGameInstance* gameInstance =  Cast<ULDGameInstance>(controller->GetGameInstance());
+	if (nullptr == gameInstance)
+	{
+		return false;
+	}
+
+	int32	error = pkt.error();
+	FString ticket = pkt.id_token().c_str();
+
+	gameInstance->mTicket = ticket;
+	
+	AGameModeBase* gameMode = session->GetWorld()->GetAuthGameMode();
+	ANetworkGameMode* networkGameMode = Cast<ANetworkGameMode>(gameMode);
+
+	//networkGameMode->RequestTravelLevel("L_SelectCharacter");
+	networkGameMode->RequestTravelLevel("L_CustomCharacter");
+
+	return true;
+}
+
+bool Handle_S2C_Singup(ANetworkController* session, Protocol::S2C_Singup& pkt)
+{
+	return true;
+}
+
+bool Handle_S2C_EmailVerified(ANetworkController* session, Protocol::S2C_EmailVerified& pkt)
+{
+	return true;
+}
