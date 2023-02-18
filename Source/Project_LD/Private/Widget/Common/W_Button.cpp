4f511d4512a72edef6e26c190c53c7ebc0467bfb
Apeirogon99 <Apeirogon99@gmail.com>
2023-02-18 11:02:22 +0900

    [gwanho] Add Button Widget
    공용 버튼 위젯
    아직 테스트중인데 잘 작동함

---
+++ Source/Project_LD/Private/Widget/Common/W_Button.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widget/Common/W_Button.h"
+#include <Components/Button.h>
+#include <Components/TextBlock.h>
+
+void UW_Button::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	mButton = Cast<UButton>(GetWidgetFromName(TEXT("mButton")));
+
+	mButtonText = Cast<UTextBlock>(GetWidgetFromName(TEXT("mButtonText")));
+
+	if (mButton != nullptr)
+	{
+		mButton->OnClicked.AddDynamic(this, &UW_Button::Click_Button);
+	}
+}
+
+void UW_Button::NativeDestruct()
+{
+	bool isBound = mButtonDelegate.IsBound();
+	if (false == isBound)
+	{
+		return;
+	}
+
+	mButtonDelegate.Unbind();
+}
+
+void UW_Button::SetText(const FString& inText)
+{
+	if (mButtonText != nullptr)
+	{
+		FText text = FText::FromString(inText);
+		mButtonText->SetText(text);
+	}
+}
+
+void UW_Button::Click_Button()
+{
+	bool isBound = mButtonDelegate.IsBound();
+	if (true == isBound)
+	{
+		mButtonDelegate.Execute();
+	}
+}
