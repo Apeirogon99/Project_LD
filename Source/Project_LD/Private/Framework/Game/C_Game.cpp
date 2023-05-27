81f7289b3d797b3bbc0c6486d469d0011ec25c85
chickenbuger <gksdidxornjs@naver.com>
2023-05-28 02:01:38 +0900

    [Jinsoo] Update C_Game Delete GameCharacter

--- Source/Project_LD/Private/Framework/Game/C_Game.cpp
+++ Source/Project_LD/Private/Framework/Game/C_Game.cpp
@@ -1,12 +1,84 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Framework/Game/C_Game.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+#include "GameContent/Item/ItemParent.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 AC_Game::AC_Game()
 {
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.f);
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 640.f, 0.f);
+	GetCharacterMovement()->bConstrainToPlane = true;
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->SetUsingAbsoluteRotation(true);
+	CameraBoom->TargetArmLength = 1600.f;
+	CameraBoom->SetRelativeRotation(FRotator(-60.f, 0.f, 0.f));
+	CameraBoom->bDoCollisionTest = false;
+
+	GameCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("TopDownCamera"));
+	GameCameraComponent->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	GameCameraComponent->bUsePawnControlRotation = false;
+
+	GetCapsuleComponent()->SetCollisionProfileName(TEXT("PlayerCharacter"));
+
+	/**Character Status Setting**/
+
+	//Attack Range
+	AttackRange = 200.0f;	AttackRadius = 50.0f;
+	//Attack Info
+	IsAttacking = false;
+	//Health
+	MaxHealth = 100.0f;	CurrentHealth = 100.0f;
+	//Mana
+	MaxMana = 100.0f;		CurrentMana = 100.0f;
+	//Character Level
+	CharacterLevel = 1;
+
+	InventoryComponent = CreateDefaultSubobject<UACInventoryComponent>(TEXT("InventoryComponent"));
+
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = true;
 }
 
 AC_Game::~AC_Game()
 {
+}
+
+void AC_Game::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("InteractItem", IE_Pressed, this, &AC_Game::InteractItem);
+}
+
+void AC_Game::InteractItem()
+{
+	TArray<AActor*> Result;
+	GetOverlappingActors(Result, AItemParent::StaticClass());
+
+	for (auto& Actor : Result)
+	{
+		if (UKismetSystemLibrary::DoesImplementInterface(Actor, UInventoryInterface::StaticClass()))
+		{
+			AItemParent* Item = Cast<AItemParent>(Actor);
+			if (Item != nullptr)
+			{
+				Item->PickUpItem();
+			}
+		}
+	}
 }
--- Source/Project_LD/Public/Framework/Game/C_Game.h
+++ Source/Project_LD/Public/Framework/Game/C_Game.h
@@ -4,14 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "Framework/Character/AppearanceCharacter.h"
-#include <Framework/Character/GameCharacter.h>
 #include "C_Game.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class PROJECT_LD_API AC_Game : public AGameCharacter
+class PROJECT_LD_API AC_Game : public AAppearanceCharacter
 {
 	GENERATED_BODY()
 
@@ -19,4 +18,46 @@ public:
 	AC_Game();
 	~AC_Game();
 	
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	/** Top down camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+		class UCameraComponent* GameCameraComponent;
+
+	/** Camera boom positioning the camera above the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+		class USpringArmComponent* CameraBoom;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Component", meta = (AllowPrivateAccess = "true"))
+		class UACInventoryComponent* InventoryComponent;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Attack", meta = (AllowPrivateAccess = "true"))
+		float AttackRange;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Attack", meta = (AllowPrivateAccess = "true"))
+		float AttackRadius;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Health", meta = (AllowPrivateAccess = "true"))
+		float MaxHealth;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Health", meta = (AllowPrivateAccess = "true"))
+		float CurrentHealth;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Mana", meta = (AllowPrivateAccess = "true"))
+		float MaxMana;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Mana", meta = (AllowPrivateAccess = "true"))
+		float CurrentMana;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Level", meta = (AllowPrivateAccess = "true"))
+		int32 CharacterLevel;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Condition", meta = (AllowPrivateAccess = "true"))
+		bool IsAttacking;
+
+public:
+	UFUNCTION()
+		void InteractItem();
 };
