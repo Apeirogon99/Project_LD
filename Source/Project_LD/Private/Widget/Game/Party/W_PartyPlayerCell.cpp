eb96e3a58a2431ad788d7c3d075d53a826754621
Apeirogon99 <Apeirogon99@gmail.com>
2023-08-12 01:46:58 +0900

    [gwanho] Update Party

--- Source/Project_LD/Private/Widget/Game/Party/W_PartyPlayerCell.cpp
+++ Source/Project_LD/Private/Widget/Game/Party/W_PartyPlayerCell.cpp
@@ -1,5 +1,127 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "Widget/Game/Party/W_PartyPlayerCell.h"
+#include <Struct/Game/CharacterDatas.h>
+#include <Components/TextBlock.h>
+#include <Components/Button.h>
+#include <Components/Image.h>
 
+void UW_PartyPlayerCell::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	mPlayerInfo = Cast<UTextBlock>(GetWidgetFromName(TEXT("mPlayerInfo")));
+
+	mActionButtonA	= Cast<UButton>(GetWidgetFromName(TEXT("mActionButtonA")));
+	mActionButtonB	= Cast<UButton>(GetWidgetFromName(TEXT("mActionButtonB")));
+
+	mClassImage		= Cast<UImage>(GetWidgetFromName(TEXT("mClassImage")));
+	mActionImageA	= Cast<UImage>(GetWidgetFromName(TEXT("mActionImageA")));
+	mActionImageB	= Cast<UImage>(GetWidgetFromName(TEXT("mActionImageB")));
+	mLeaderImage	= Cast<UImage>(GetWidgetFromName(TEXT("mLeaderImage")));
+
+	if (mActionButtonA != nullptr)
+	{
+		mActionButtonA->OnClicked.AddUniqueDynamic(this, &UW_PartyPlayerCell::Click_ActionButtonA);
+	}
+
+	if (mActionButtonB != nullptr)
+	{
+		mActionButtonB->OnClicked.AddUniqueDynamic(this, &UW_PartyPlayerCell::Click_ActionButtonB);
+	}
+
+	mRemoteID = 0;
+}
+
+void UW_PartyPlayerCell::Click_ActionButtonA()
+{
+	bool isBound = mActionButtonDelegateA.IsBound();
+	if (true == isBound)
+	{
+		mActionButtonDelegateA.Execute();
+	}
+}
+
+void UW_PartyPlayerCell::Click_ActionButtonB()
+{
+	bool isBound = mActionButtonDelegateB.IsBound();
+	if (true == isBound)
+	{
+		mActionButtonDelegateB.Execute();
+	}
+}
+
+void UW_PartyPlayerCell::SetPlayerInfo(const int64& inRemoteID, const int32& inLevel, const int32& inClass, const FString& inPlayerName)
+{
+	FString tempText;
+	tempText.Append(FString::FromInt(inLevel));
+	tempText.Append(TEXT("레벨 "));
+
+	ECharacterClass characterClass = StaticCast<ECharacterClass>(inClass);
+	switch (characterClass)
+	{
+	case ECharacterClass::None:
+		tempText.Append(TEXT("미지정 "));
+		//
+		break;
+	case ECharacterClass::Warrior:
+		tempText.Append(TEXT("성기사 "));
+		mClassImage->SetBrushFromTexture(mWarriorClassTexture);
+		break;
+	case ECharacterClass::Wizard:
+		tempText.Append(TEXT("마법사 "));
+		mClassImage->SetBrushFromTexture(mWizardClassTexture);
+		break;
+	default:
+		break;
+	}
+	tempText.Append(inPlayerName);
+
+	FText netText = FText::FromString(tempText);
+	mPlayerInfo->SetText(netText);
+	mRemoteID = inRemoteID;
+}
+
+void UW_PartyPlayerCell::SetListType(const EPartyListType& inPartyListType, const bool& inVisible)
+{
+	if (false == inVisible)
+	{
+		mActionImageA->SetVisibility(ESlateVisibility::Hidden);
+		mActionImageB->SetVisibility(ESlateVisibility::Hidden);
+		return;
+	}
+	else
+	{
+		mActionImageA->SetVisibility(ESlateVisibility::Visible);
+		mActionImageB->SetVisibility(ESlateVisibility::Visible);
+	}
+
+	switch (inPartyListType)
+	{
+	case EPartyListType::None:
+		break;
+	case EPartyListType::PlayerList:
+		mActionImageA->SetBrushFromTexture(mLeaderTexture);
+		mActionImageB->SetBrushFromTexture(mKickOutTexture);
+		break;
+	case EPartyListType::RequestList:
+		mActionImageA->SetBrushFromTexture(mAcceptTexture);
+		mActionImageB->SetBrushFromTexture(mCancleTexture);
+		break;
+	default:
+		break;
+	}
+}
+
+void UW_PartyPlayerCell::SetLeader(const bool& inIsLeader)
+{
+	if (inIsLeader)
+	{
+		mLeaderImage->SetVisibility(ESlateVisibility::Visible);
+	}
+	else
+	{
+		mLeaderImage->SetVisibility(ESlateVisibility::Hidden);
+	}
+}
--- Source/Project_LD/Public/Widget/Game/Party/W_PartyPlayerCell.h
+++ Source/Project_LD/Public/Widget/Game/Party/W_PartyPlayerCell.h
@@ -6,6 +6,20 @@
 #include "Blueprint/UserWidget.h"
 #include "W_PartyPlayerCell.generated.h"
 
+UENUM(BlueprintType)
+enum class EPartyListType : uint8
+{
+	None			UMETA(DisplayName = "None"),
+	PlayerList 		UMETA(DisplayName = "Invite"),
+	RequestList		UMETA(DisplayName = "Request"),
+};
+
+class UTextBlock;
+class UImage;
+class UButton;
+
+DECLARE_DELEGATE(FPartyActionButtonDelegate);
+
 /**
  * 
  */
@@ -14,4 +28,75 @@ class PROJECT_LD_API UW_PartyPlayerCell : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+	//virtual void NativeOnInitialized() override;
+	//virtual void NativePreConstruct() override;
+	virtual void NativeConstruct() override;
+	//virtual void NativeDestruct() override;
+	//virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime);
+
+public:
+
+	UPROPERTY(Meta = (BindWidget))
+		UImage* mClassImage;
+
+	UPROPERTY(Meta = (BindWidget))
+		UTextBlock* mPlayerInfo;
+
+	UPROPERTY(Meta = (BindWidget))
+		UButton* mActionButtonA;
+
+	UPROPERTY(Meta = (BindWidget))
+		UButton* mActionButtonB;
+
+	UPROPERTY(Meta = (BindWidget))
+		UImage* mActionImageA;
+
+	UPROPERTY(Meta = (BindWidget))
+		UImage* mActionImageB;
+
+	UPROPERTY(Meta = (BindWidget))
+		UImage* mLeaderImage;
+
+public:
+	UFUNCTION()
+		void Click_ActionButtonA();
+
+	UFUNCTION()
+		void Click_ActionButtonB();
+
+public:
+	UFUNCTION()
+		void SetPlayerInfo(const int64& inRemoteID, const int32& inLevel, const int32& inClass, const FString& inPlayerName);
+	
+	UFUNCTION()
+		void SetListType(const EPartyListType& inPartyListType, const bool& inVisible);
+
+	UFUNCTION()
+		void SetLeader(const bool& inIsLeader);
+
+public:
+	FPartyActionButtonDelegate mActionButtonDelegateA;
+	FPartyActionButtonDelegate mActionButtonDelegateB;
+
+	UPROPERTY(EditAnywhere, Category = "Widget")
+		UTexture2D* mWarriorClassTexture;
+
+	UPROPERTY(EditAnywhere, Category = "Widget")
+		UTexture2D* mWizardClassTexture;
+
+	UPROPERTY(EditAnywhere, Category = "Widget")
+		UTexture2D* mLeaderTexture;
+
+	UPROPERTY(EditAnywhere, Category = "Widget")
+		UTexture2D* mKickOutTexture;
+
+	UPROPERTY(EditAnywhere, Category = "Widget")
+		UTexture2D* mAcceptTexture;
+
+	UPROPERTY(EditAnywhere, Category = "Widget")
+		UTexture2D* mCancleTexture;
+
+	UPROPERTY()
+		int64	mRemoteID;
 };
